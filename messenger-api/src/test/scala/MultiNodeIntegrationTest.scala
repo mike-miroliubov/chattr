c5609cf36c3b6f27fb0ea25dd13c27cef1ff9c48
Mikhail Miroliubov <mike.miroliubov@gmail.com>
2025-06-12 22:22:53 -0400

    Fixing tests

--- messenger-api/src/test/scala/MultiNodeIntegrationTest.scala
+++ messenger-api/src/test/scala/MultiNodeIntegrationTest.scala
@@ -13,7 +13,7 @@ import org.apache.pekko.http.scaladsl.Http
 import org.apache.pekko.http.scaladsl.model.ws.{Message, TextMessage, WebSocketRequest}
 import org.apache.pekko.stream.scaladsl.{Flow, Keep, Sink, Source}
 import org.scalatest.BeforeAndAfterAll
-import org.scalatest.flatspec.{AnyFlatSpec, AsyncFlatSpec}
+import org.scalatest.flatspec.AsyncFlatSpec
 import spray.json.*
 
 import java.net.ServerSocket
@@ -27,111 +27,121 @@ class MultiNodeIntegrationTest extends AsyncFlatSpec with BeforeAndAfterAll with
   private val seedNodePort = findFreePort()
   private val config1 = NodeConfig(seedNodePort)
   private val server = Http()(using config1.system).newServerAt("localhost", 0)
-  private val binding1 = Await.result(
-    server.bind(Api(using config1.system, config1.system.executionContext).handleWsRequest), 5.seconds)
-
-  Thread.sleep(1000)
+  private val binding1 = server.bind(Api(using config1.system, config1.system.executionContext).handleWsRequest)
 
   private val clusterPort2 = findFreePort()
   private val config2 = NodeConfig(clusterPort2)
   private val server2 = Http()(using config2.system).newServerAt("localhost", 0)
-  private val binding2 = Await.result(
-    server.bind(Api(using config2.system, config2.system.executionContext).handleWsRequest), 5.seconds)
-
-  Thread.sleep(1000)
+  private val binding2 = server.bind(Api(using config2.system, config2.system.executionContext).handleWsRequest)
 
   implicit val clientSystem: ActorSystem[_] = ActorSystem(Behaviors.empty, "test-system", ConfigFactory.load("application-client-test.conf"))
 
   "Clients" should "connect to different nodes" in {
-    val clientSource1 = Source.queue[InputMessageDto](3)
-    val clientFlow1 = Http().webSocketClientFlow(WebSocketRequest(s"ws:/${binding1.localAddress}/api/connect?userName=foo"))
-    val clientSink1 = Flow[String].take(1).toMat(Sink.seq[String])(Keep.right)
+    Future.sequence(Seq(binding1, binding2)).flatMap { case Seq(b1, b2) =>
+      Thread.sleep(1000)
 
-    val client2Source = Source.queue[String](1)
-    val clientFlow2 = Http().webSocketClientFlow(WebSocketRequest(s"ws:/${binding2.localAddress}/api/connect?userName=bar"))
-    val clientSink2 = Flow[String].take(4).toMat(Sink.seq[String])(Keep.right)
+      val clientSource1 = Source.queue[InputMessageDto](3)
+      val clientFlow1 = Http().webSocketClientFlow(WebSocketRequest(s"ws:/${b1.localAddress}/api/connect?userName=foo"))
+      val clientSink1 = Flow[String].take(1).toMat(Sink.seq[String])(Keep.right)
 
-    // Client 1 connects
-    val (client1In, client1Out) = clientSource1
-      .map { input => TextMessage(input.toJson.toString) }
-      .viaMat(clientFlow1)(Keep.left)
-      .map { _.asTextMessage.getStrictText }
-      .toMat(clientSink1)(Keep.both)
-      .run()
+      val client2Source = Source.queue[String](1)
+      val clientFlow2 = Http().webSocketClientFlow(WebSocketRequest(s"ws:/${b2.localAddress}/api/connect?userName=bar"))
+      val clientSink2 = Flow[String].take(4).toMat(Sink.seq[String])(Keep.right)
 
-    // Client 2 connects
-    val (client2In, client2Out) = client2Source
-      .map { input => TextMessage(input) }
-      .viaMat(clientFlow2)(Keep.left)
-      .map { _.asTextMessage.getStrictText }
-      .toMat(clientSink2)(Keep.both)
-      .run()
+      // Client 1 connects
+      val (client1In, client1Out) = clientSource1
+        .map { input => TextMessage(input.toJson.toString) }
+        .viaMat(clientFlow1)(Keep.left)
+        .map { _.asTextMessage.getStrictText }
+        .toMat(clientSink1)(Keep.both)
+        .run()
 
-    // when
-    // Client 1 sends 3 messages
-    client1In.offer(InputMessageDto("1", "bar", "hi"))
-    client1In.offer(InputMessageDto("2", "bar", "hey"))
-    client1In.offer(InputMessageDto("3", "bar", "yo"))
+      // Client 2 connects
+      val (client2In, client2Out) = client2Source
+        .map { input => TextMessage(input) }
+        .viaMat(clientFlow2)(Keep.left)
+        .map { _.asTextMessage.getStrictText }
+        .toMat(clientSink2)(Keep.both)
+        .run()
 
-    // then
-    assert(Await.result(client1Out, 5.seconds) == Seq(
-      """{"from":"","id":"","text":"You joined the chat"}"""
-    ))
+      // when
+      // Client 1 sends 3 messages
+      client1In.offer(InputMessageDto("1", "bar", "hi"))
+      client1In.offer(InputMessageDto("2", "bar", "hey"))
+      client1In.offer(InputMessageDto("3", "bar", "yo"))
 
-    assert(Await.result(client2Out, 5.seconds) == Seq(
-      """{"from":"","id":"","text":"You joined the chat"}""",
-      """{"from":"foo","id":"1","text":"hi"}""",
-      """{"from":"foo","id":"2","text":"hey"}""",
-      """{"from":"foo","id":"3","text":"yo"}"""
-    ))
+      // then
+      Future.sequence(Seq(client1Out, client2Out)).map {
+        case Seq(c1, c2) => (c1, c2, client1In, client2In)
+      }
+    }
+    .map { case (c1, c2, client1In, client2In) =>
+      assert(c1 == Seq(
+        """{"from":"","id":"","text":"You joined the chat"}"""
+      ))
 
-    client1In.complete()
-    client2In.complete()
+      assert(c2 == Seq(
+        """{"from":"","id":"","text":"You joined the chat"}""",
+        """{"from":"foo","id":"1","text":"hi"}""",
+        """{"from":"foo","id":"2","text":"hey"}""",
+        """{"from":"foo","id":"3","text":"yo"}"""
+      ))
+      client1In.complete()
+      client2In.complete()
 
-    succeed
+      succeed
+    }
   }
 
   "Clients" should "create group and message in a group" in {
-    // given
     val groupName = UUID.randomUUID().toString
-    val receiveMessage = Flow[Message].map(m => m.asTextMessage.getStrictText)
 
-    val clientSource1 = Source.queue[InputMessageDto](3)
-    val clientFlow1 = Http().webSocketClientFlow(WebSocketRequest(s"ws:/${binding1.localAddress}/api/connect?userName=user1"))
-    val clientSink1 = receiveMessage.take(7).toMat(Sink.seq[String])(Keep.right)
+    Future.sequence(Seq(binding1, binding2)).flatMap { case Seq(b1, b2) =>
+      Thread.sleep(1000)
+      // given
+      val receiveMessage = Flow[Message].map(m => m.asTextMessage.getStrictText)
 
-    val clientSource2 = Source.queue[InputMessageDto](3)
-    val clientFlow2 = Http().webSocketClientFlow(WebSocketRequest(s"ws:/${binding2.localAddress}/api/connect?userName=user2"))
-    val clientSink2 = receiveMessage.take(7).toMat(Sink.seq[String])(Keep.right)
+      val clientSource1 = Source.queue[InputMessageDto](3)
+      val clientFlow1 = Http().webSocketClientFlow(WebSocketRequest(s"ws:/${b1.localAddress}/api/connect?userName=user1"))
+      val clientSink1 = receiveMessage.take(7).toMat(Sink.seq[String])(Keep.right)
 
-    val clientSource3 = Source.queue[InputMessageDto](3)
-    val clientFlow3 = Http().webSocketClientFlow(WebSocketRequest(s"ws:/${binding2.localAddress}/api/connect?userName=user3"))
-    val clientSink3 = receiveMessage.take(7).toMat(Sink.seq[String])(Keep.right)
+      val clientSource2 = Source.queue[InputMessageDto](3)
+      val clientFlow2 = Http().webSocketClientFlow(WebSocketRequest(s"ws:/${b2.localAddress}/api/connect?userName=user2"))
+      val clientSink2 = receiveMessage.take(7).toMat(Sink.seq[String])(Keep.right)
 
-    config1.groupSharding ! ShardingEnvelope(s"g#$groupName", MakeGroup("user1", Set()))
-    config1.groupSharding ! ShardingEnvelope(s"g#$groupName", AddMember("user2"))
-    config1.groupSharding ! ShardingEnvelope(s"g#$groupName", AddMember("user3"))
+      val clientSource3 = Source.queue[InputMessageDto](3)
+      val clientFlow3 = Http().webSocketClientFlow(WebSocketRequest(s"ws:/${b2.localAddress}/api/connect?userName=user3"))
+      val clientSink3 = receiveMessage.take(7).toMat(Sink.seq[String])(Keep.right)
 
-    def publishMessage(i: InputMessageDto) = TextMessage(i.toJson.toString)
+      config1.groupSharding ! ShardingEnvelope(s"g#$groupName", MakeGroup("user1", Set()))
+      config1.groupSharding ! ShardingEnvelope(s"g#$groupName", AddMember("user2"))
+      config1.groupSharding ! ShardingEnvelope(s"g#$groupName", AddMember("user3"))
 
-    // connect 3 clients
-    val ((client1In, client1Connected), client1Out) = clientSource1
-      .map(publishMessage)
-      .viaMat(clientFlow1)(Keep.both)
-      .toMat(clientSink1)(Keep.both).run()
+      def publishMessage(i: InputMessageDto) = TextMessage(i.toJson.toString)
 
-    val ((client2In, client2Connected), client2Out) = clientSource2
-      .map(publishMessage)
-      .viaMat(clientFlow2)(Keep.both)
-      .toMat(clientSink2)(Keep.both).run()
+      // connect 3 clients
+      val ((client1In, client1Connected), client1Out) = clientSource1
+        .map(publishMessage)
+        .viaMat(clientFlow1)(Keep.both)
+        .toMat(clientSink1)(Keep.both).run()
 
-    val ((client3In, client3Connected), client3Out) = clientSource3
-      .map(publishMessage)
-      .viaMat(clientFlow3)(Keep.both)
-      .toMat(clientSink3)(Keep.both).run()
+      val ((client2In, client2Connected), client2Out) = clientSource2
+        .map(publishMessage)
+        .viaMat(clientFlow2)(Keep.both)
+        .toMat(clientSink2)(Keep.both).run()
 
+      val ((client3In, client3Connected), client3Out) = clientSource3
+        .map(publishMessage)
+        .viaMat(clientFlow3)(Keep.both)
+        .toMat(clientSink3)(Keep.both).run()
+
+      Future.sequence(Seq(client1Connected, client2Connected, client3Connected))
+      .map { _ =>
+        (client1In, client2In, client3In, client1Out, client2Out, client3Out)
+      }
+    }
     // wait until all clients connect
-    Future.sequence(Seq(client1Connected, client2Connected, client3Connected)).flatMap { _ =>
+    .flatMap { (client1In, client2In, client3In, client1Out, client2Out, client3Out) =>
       // when
       for {
         (client, clientId) <- Seq(client1In, client2In, client3In).zipWithIndex
@@ -141,8 +151,13 @@ class MultiNodeIntegrationTest extends AsyncFlatSpec with BeforeAndAfterAll with
       }
 
       // then
-      val assert1 = client1Out.flatMap { s =>
-        assert(s.toSet == Set( // messages can come out of order
+
+      for {
+        c1 <- client1Out
+        c2 <- client2Out
+        c3 <- client3Out
+      } yield {
+        assert(c1.toSet == Set( // messages can come out of order
           """{"from":"","id":"","text":"You joined the chat"}""",
           """{"from":"user2","id":"2-1","text":"hey!"}""",
           """{"from":"user2","id":"2-2","text":"ho!"}""",
@@ -151,10 +166,8 @@ class MultiNodeIntegrationTest extends AsyncFlatSpec with BeforeAndAfterAll with
           """{"from":"user3","id":"3-2","text":"ho!"}""",
           """{"from":"user3","id":"3-3","text":"lets go!"}"""
         ))
-      }
 
-      val assert2 = client2Out.flatMap { s =>
-        assert(s.toSet == Set(
+        assert(c2.toSet == Set(
           """{"from":"","id":"","text":"You joined the chat"}""",
           """{"from":"user1","id":"1-1","text":"hey!"}""",
           """{"from":"user1","id":"1-2","text":"ho!"}""",
@@ -163,10 +176,8 @@ class MultiNodeIntegrationTest extends AsyncFlatSpec with BeforeAndAfterAll with
           """{"from":"user3","id":"3-2","text":"ho!"}""",
           """{"from":"user3","id":"3-3","text":"lets go!"}"""
         ))
-      }
 
-      val assert3 = client3Out.flatMap { s =>
-        assert(s.toSet == Set(
+        assert(c3.toSet == Set(
           """{"from":"","id":"","text":"You joined the chat"}""",
           """{"from":"user1","id":"1-1","text":"hey!"}""",
           """{"from":"user1","id":"1-2","text":"ho!"}""",
@@ -175,15 +186,13 @@ class MultiNodeIntegrationTest extends AsyncFlatSpec with BeforeAndAfterAll with
           """{"from":"user2","id":"2-2","text":"ho!"}""",
           """{"from":"user2","id":"2-3","text":"lets go!"}"""
         ))
+
+        client1In.complete()
+        client2In.complete()
+        client3In.complete()
+
+        succeed
       }
-
-      Future.sequence(Seq(assert1, assert2, assert3))
-    }.flatMap { _ =>
-      client1In.complete()
-      client2In.complete()
-      client3In.complete()
-
-      succeed
     }
   }
 
